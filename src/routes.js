5450c8ce9880d653d41066e3ec5cc320de7ce592
icarofs <icaroferreira.souza@gmail.com>
2020-01-31 16:26:44 -0300

    configuracao eslint, prettier e editorconfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Teste" });
-});
+routes.get('/', (req, res) => res.json({ message: 'Teste' }));
 
 export default routes;
