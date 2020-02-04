e6f59ac717482a66581f04e9b5374624b390ab7f
icarofs <icaroferreira.souza@gmail.com>
2020-02-04 17:37:08 -0300

    criacao cadastro de usuarios

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,19 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExistis = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (userExistis) {
+      return res.status(400).json({ error: 'User already existis.' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Ícaro Ferreira de Souza',
-    email: 'icaroferreira.souza@gmail.com',
-    password_hash: '123456',
-  });
+routes.post('/users', UserController.store);
 
-  res.json(user);
-});
 export default routes;
