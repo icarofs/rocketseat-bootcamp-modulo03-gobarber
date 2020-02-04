399aaabcaec4050d6e4c649ca6e0541a38b5fbca
icarofs <icaroferreira.souza@gmail.com>
2020-02-04 16:42:19 -0300

    criacao de loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Teste' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Ícaro Ferreira de Souza',
+    email: 'icaroferreira.souza@gmail.com',
+    password_hash: '123456',
+  });
 
+  res.json(user);
+});
 export default routes;
