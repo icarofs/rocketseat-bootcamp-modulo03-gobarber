c27ccdb3ea7e0e56d323bf8b32815693b38b37a8
icarofs <icaroferreira.souza@gmail.com>
2020-02-04 21:02:06 -0300

    update de usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,9 +16,25 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassword } = req.body;
 
-    return res.json({ ok: true });
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExistis = await User.findOne({ where: { email } });
+
+      if (userExistis) {
+        return res.status(400).json({ error: 'User already existis.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
