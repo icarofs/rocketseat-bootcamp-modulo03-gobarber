feefd1b954997dd5eb60959b91300565fd488856
icarofs <icaroferreira.souza@gmail.com>
2020-02-04 22:17:38 -0300

    validacao dados de entrada

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -7,7 +7,7 @@ export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.send(401).json({ error: 'Token not provided' });
+    return res.status(401).json({ error: 'Token not provided' });
   }
 
   const [, token] = authHeader.split(' ');
@@ -19,8 +19,6 @@ export default async (req, res, next) => {
 
     return next();
   } catch (err) {
-    return res.send(401).json({ error: 'Token invalid' });
+    return res.status(401).json({ error: 'Token invalid' });
   }
-
-  // return next();
 };
