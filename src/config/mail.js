3b3ca3ba5cbdfc301adf48bfa5b130852eb562a1
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-02-07 18:11:32 -0300

    configuracao nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '3b9bfb4c508f9b',
+    pass: 'd0f30a8344f778',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
